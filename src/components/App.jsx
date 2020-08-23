f1b15eab8a4f83488959801d1ea8101a426046e6
clf94-dev <clf94@hotmail.es>
2020-08-23 16:17:13 +0200

    footer added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './styles/App.css';
 import Home from './pages/Home.jsx'
-function App  ()  {
+function App() {
     return (
         <div>
-    <Router>     
-          <Navbar/>  
-          <Switch>
-            <Route path = '/' exact component={Home}></Route>
-          </Switch>
-          </Router> 
+            <Router>
+                <Navbar/>
+                <Switch>
+                    <Route path='/' exact component={Home}></Route>
+                </Switch>
+                <Footer/>
+            </Router>
         </div>
     );
 }
