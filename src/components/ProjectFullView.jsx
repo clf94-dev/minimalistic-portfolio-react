2610b5c8043d64c27636193a579ae59207f31915
clf94-dev <clf94@hotmail.es>
2020-09-02 09:37:41 +0200

    fixed next-previous button component

--- src/components/ProjectFullView.jsx
+++ src/components/ProjectFullView.jsx
@@ -22,74 +22,81 @@ import BookmarkStatic2 from './images/detail/desktop/image-bookmark-preview-2.jp
 import InsureStatic2 from './images/detail/desktop/image-insure-preview-2.jpg';
 import FyloStatic2 from './images/detail/desktop/image-fylo-preview-2.jpg';
 
-
 export default function ProjectFullView(props) {
     const ProjectTopImg = [Manage, Bookmark, Insure, Fylo];
     const ProjectStaticImg = [ManageStatic, BookmarkStatic, InsureStatic, FyloStatic];
     const ProjectStaticImg2 = [ManageStatic2, BookmarkStatic2, InsureStatic2, FyloStatic2];
+    const previousProject = [3, 0, 1, 2];
+    const nextProject = [1, 2, 3, 0];
+
     return (
 
         <div>
-        <Grid  container direction='column' className='full-project-cont'>
-            <img
-                className='project-top'
-                src={ProjectTopImg[props.index]}
-                alt="project hero"/>
-            <Grid container direction="row" className='description-row'>
-                <Grid item sm={6} xs={12} direction='column' className='left-text'>
-                    <hr/>
-                    <h2>{PortfolioFullProj[props.index].titleRight}</h2>
-                    <p>{PortfolioFullProj[props.index].textRight}</p>
-                    <h5>{PortfolioFullProj[props.index].designType}</h5>
-                    <h5>{PortfolioFullProj[props.index].languages}</h5>
-                    <Button
-                        className='project-btn'
-                        link=""
-                        buttonStyle='btn--outline'
-                        buttonSize='btn--large'>
-                        VIEW WEBSITE
-                    </Button>
-                    <hr/>
-                </Grid>
-                <Grid item sm={6} xs={12} direction='column' className='right-text'>
-                    <Grid container className='text-cont' direction='colum'>
-                        <h3>{PortfolioFullProj[props.index].titleLeft}</h3>
-                        <p>{PortfolioFullProj[props.index].textLeft}</p>
-                        <h3>{PortfolioFullProj[props.index].staticTitle}</h3>
+            <Grid container direction='column' className='full-project-cont'>
+                <img
+                    className='project-top'
+                    src={ProjectTopImg[props.index]}
+                    alt="project hero"/>
+                <Grid container direction="row" className='description-row'>
+                    <Grid item sm={6} xs={12} direction='column' className='left-text'>
+                        <hr/>
+                        <h2>{PortfolioFullProj[props.index].titleRight}</h2>
+                        <p>{PortfolioFullProj[props.index].textRight}</p>
+                        <h5>{PortfolioFullProj[props.index].designType}</h5>
+                        <h5>{PortfolioFullProj[props.index].languages}</h5>
+                        <Button
+                            className='project-btn'
+                            link=""
+                            buttonStyle='btn--outline'
+                            buttonSize='btn--large'>
+                            VIEW WEBSITE
+                        </Button>
+                        <hr/>
                     </Grid>
-                    <img src={ProjectStaticImg[props.index]} alt="static preview 1"/>
-                    <img src={ProjectStaticImg2[props.index]} alt="static preview 2"/>
+                    <Grid item sm={6} xs={12} direction='column' className='right-text'>
+                        <Grid container className='text-cont' direction='colum'>
+                            <h3>{PortfolioFullProj[props.index].titleLeft}</h3>
+                            <p>{PortfolioFullProj[props.index].textLeft}</p>
+                            <h3>{PortfolioFullProj[props.index].staticTitle}</h3>
+                        </Grid>
+                        <img src={ProjectStaticImg[props.index]} alt="static preview 1"/>
+                        <img src={ProjectStaticImg2[props.index]} alt="static preview 2"/>
+                    </Grid>
+                </Grid>
+
+                <Grid container direction='row' className='next-prev-proj'>
+                    <Grid item sm={6} xs={6} direction='row' className='column-left'>
+                        <Grid container direction='row'>
+                            <Grid item md={2} xs={4} direction='column'>
+                                <Button
+                                    buttonStyle='btn--outline'
+                                    link={`/full-project/${previousProject[props.index]}`}><img className='previous-next-btn' src={ArrowLeft} alt="arrow left"/></Button>
+                            </Grid>
+                            <Grid item md={10} xs={8} direction='column' className='previous-text'>
+                                <h5>Previous Project</h5>
+                                <h3>{PortfolioFullProj[previousProject[props.index]].titleRight}</h3>
+
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item sm={6} xs={6} direction='row' className='column-right'>
+                        <Grid container direction='row'>
+                            <Grid item md={10} xs={8} direction='column' className='next-text'>
+                                <h5>Next Project</h5>
+
+                                <h3>{PortfolioFullProj[nextProject[props.index]].titleRight}</h3>
+
+                            </Grid>
+                            <Grid item md={2} xs={4} direction='column' style={{textAlign: 'end'}}>
+                                <Button
+                                    buttonStyle='btn--outline'
+                                    link={`/full-project/${nextProject[props.index]}`}><img className='previous-next-btn' src={ArrowRight} alt="arrow right"/></Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
 
-            <Grid container direction='row' className='next-prev-proj'>
-                <Grid item  sm={6} xs={12} direction='row' className='column-left'>
-                    <Grid item sm={6} xs={12} direction='column'>
-                        <Button ><img src={ArrowLeft} alt=""/></Button>
-                    </Grid>
-                    <Grid item sm={6} xs={12} direction='column'>
-                        <h5>Previous Project</h5>
-                        <h3>{PortfolioFullProj[props.index == 0? 3  : props.index - 1].titleRight}</h3>
-
-                    </Grid>
-                </Grid>
-                <Grid item sm={6} xs={12} direction='row' className='column-right' >
-         
-                    <Grid item sm={6} xs={12} direction='column'>
-                        <h5>Next Project</h5>
-                        
-                        <h3>{PortfolioFullProj[props.index == 0 ? 3 : props.index - 1].titleRight}</h3>
-
-                    </Grid>
-                    <Grid item sm={6} xs={12} direction='column'>
-                        <Button ><img src={ArrowRight} alt=""/></Button>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </Grid>
-
-     </div>
+        </div>
     )
 }
-
- 
